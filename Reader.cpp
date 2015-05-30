8ccf785beba19f650453aa29fc1f53cc5901777d
itaynoama <itay.noama@gmail.com>
2015-05-30 14:48:08 +0300

    n

--- Reader.cpp
+++ Reader.cpp
@@ -11,8 +11,6 @@
 
 using namespace std;
 
-#define N 250
-
 Reader::Reader(const char* file_name){
     ifstream f(file_name);
     char* line = get_line(f);
@@ -31,34 +29,8 @@ Reader::Reader(const char* file_name){
     
 }
 
-vector<string> Reader::split(const string s, char c){
-    vector<string> result;
-    int start = 1;
-    int end = 1;
-    
-    while(s[end] != ']'){
-        while(s[end] != c){
-            end++;
-        }
-        
-        result.push_back(string(s,start,end-start));
-        start = end+1;
-        end++;
-    }
-    return result;
-}
 
-char* Reader::get_line(ifstream &f) {
-    char *temp = new char[N];
-    f.getline(temp, N);
-    
-    if (temp[0] == '#') {
-        delete[] temp;
-        return get_line(f);
-    }
-    
-    return temp;
-}
+
 
 Reader::Reader() {
 	
